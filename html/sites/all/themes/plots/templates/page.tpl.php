8095ff4a249b8cf925a8323cdaf981eb59f3782e
jywarren <jeff@unterbahn.com>
2010-12-17 17:25:59 -0500

    latest edits

--- html/sites/all/themes/plots/templates/page.tpl.php
+++ html/sites/all/themes/plots/templates/page.tpl.php
@@ -134,7 +134,7 @@
 	</div> 
     </div> 
 
-    <div id="header"><div class="section clearfix">
+    <div id="header"><div class="clearfix">
 
       <?php if ($logo): ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
@@ -230,5 +230,18 @@
 
   <?php print $closure; ?>
 
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-180781-33']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 </html>
