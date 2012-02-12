12b3c9e9766a11aa07b8c3a35d9298e5016c408c
Jeffrey Warren <jeff@unterbahn.com>
2012-02-12 12:21:27 -0500

    module cleanup

--- html/modules/dashboard/dashboard.admin.js
+++
@@ -1,9 +0,0 @@
-if(Drupal.jsEnabled) {
-  $(document).ready(function(){
-    Drupal.CTools.AJAX.commands.modal_dismiss = null;
-    Drupal.CTools.AJAX.commands.modal_dismiss = function(){
-      Drupal.CTools.Modal.dismiss();
-      window.location.reload(true);
-    }
-  });
-}
--- html/modules/dashboard/templates/dashboard-browse-widgets.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php 
-  print $widget_filter;
-  foreach ($widgets as $widget):
-    print theme('dashboard_display_widget', $widget); 
-  endforeach; 
-?>
